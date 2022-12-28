60656336e637c0e2cebf79cc9dec39de69b0d643
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2022-12-28 21:13:38 +0300

    improve display with styles.css

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Books } from "./pages/Books";
 import { Add } from './pages/Add';
 import { Update } from './pages/Update';
+import "./style.css";
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Books/>}/>
-        <Route path="/add" element={<Add/>}/>
-        <Route path="/update" element={<Update/>}/>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books/>}/>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/update" element={<Update/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 import { React, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { apiBaseUrl } from './../config/config';
-
+import Edit from "../resources/images/edit.png";
+import Delete from "../resources/images/delete.png";
 export const Books = () => {
   const [books, setBooks] = useState([]);
 
@@ -26,10 +27,18 @@ export const Books = () => {
             {book.cover && <img src={book.img} alt="" />}
             <h1>{book.title}</h1>
             <p>{book.desc}</p>
+            <div className="btn-group">
+                <button className="btn delete">
+                  <img src={Delete} alt="" />
+                </button>
+                <button className="btn update">
+                  <img src={Edit} alt="" />
+                </button>
+            </div>
           </div>
         ))}
       </div>
-      <button>
+      <button className="btn-add">
         <Link to="/add">Add New Book</Link>
       </button>
     </div>
